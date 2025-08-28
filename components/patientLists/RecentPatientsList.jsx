95731c278df64dc6aeca061faa8a24eaaed60c8e
karansenthil8 <karanwebdev8@gmail.com>
2025-08-28 20:33:27 +0530

    Feat : Popup Added Succefully

--- components/patientLists/RecentPatientsList.jsx
+++ components/patientLists/RecentPatientsList.jsx
@@ -1,13 +1,17 @@
 "use client";
 import { useState } from 'react';
+import { PatientActivityCard } from '../recentPatientActivity/PatientActivityCard';
 
 
 export default function RecentPatientsList() {
 
+    const [isOpen, setIsOpen] = useState(false)
+
+
 
     return (
         <>
-
+            <PatientActivityCard />
         </>
     );
 }
